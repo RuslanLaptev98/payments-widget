b14fe3d232080adb23845445da3e4796b8d03a8d
ruslanlaptev <RLBlockchain@yandex.ru>
2022-07-20 18:32:14 +0300

    #350: add head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import PaymentsWidget from '../components/PaymentsWidget';
 
 const Home: NextPage = () => {
-  return <PaymentsWidget />;
+  return (
+    <>
+      <Head>
+        <title>Payments Widget</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <PaymentsWidget />
+    </>
+  );
 };
 
 export default Home;
